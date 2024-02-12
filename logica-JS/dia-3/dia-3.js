0f3c32542bdef00a3415a05757d4981cd6f127f8
Diogo Moura <diogoodev@gmail.com>
2024-02-11 21:36:14 -0300

    solution dia-3.js

--- logica-JS/dia-3/dia-3.js
+++ logica-JS/dia-3/dia-3.js
@@ -1,54 +1,45 @@
+
 let framework = 0;
-let fullstack = 0;
-let escolha = 0;
-let pergunta = 0;
+
 const carreira = prompt("Qual area de desenvolvimento você pretende seguir? Digite [1] para Front-End e [2] para Back-End")
-if(carreira === 1){
+if(carreira == 1){
   alert("Excelente escolha vamos ver em que você irá se especializar")
-  framework = prompt("Digite [1]se você quiser aprender React e [2]se você quiser ou aprender Vue")
-  if(framework === 1){
-    alert("React é uma excelente escolha!")
-    fullstack = prompt(' Perfeito! Agora você deve digitar [1] se quiser seguir se especializando na área escolhida ou digite [2] se quiser seguir se desenvolvendo para se tornar Fullstack')
-  }
-  else if(framework === 2){
-    alert("Vue é uma escolha maravilhosa!")
-    fullstack = prompt(' Perfeito! Agora você deve digitar [1] se quiser seguir se especializando na área escolhida ou digite [2] se quiser seguir se desenvolvendo para se tornar Fullstack')
-  }
-  escolha = prompt('Quais são as tecnologias nas quais você gostaria de se especializar ou de conhecer?')
-
-  pergunta = prompt('Tem mais alguma tecnologia que você gostaria de aprender?')
-
-  while (pergunta != 'ok') {
-    pergunta = prompt('Tem mais alguma tecnologia que você gostaria de aprender? Digite ok para sair')
-    alert('Excelente escolha')
-  }
+  framework = prompt("Digite [1]se você quiser aprender React e [2]se você quiser aprender Vue")
 }
-else if(carreira === 2){
+if(framework == 1){
+  alert("React é uma excelente escolha!")
+  
+}else if(framework == 2){
+    alert("Vue é uma escolha maravilhosa!")
+  }
+if(carreira == 2){
   alert("Excelente escolha vamos ver em que você irá se especializar")
-  const framework = prompt("Digite [1] se você quiser aprender C# e [2] se você quiser aprender Java")
-  if(framework === 1){
+  framework = prompt("Digite [1] se você quiser aprender C# e [2] se você quiser aprender Java")
+
+  if(framework == 1){
     alert("C#  é uma excelente linguagem parabéns escolha!")
   }
-  if(framework === 2){
+  if(framework == 2){
     alert("Java é uma escolha maravilhosa!")
-  }
-  
-
-  if(fullstack === 1 ){
-    alert('Parabéns você ira se tornar um especialista na linguagem escolhida escolhido')
-  } 
-  if(fullstack === 2){
-    alert('Parabéns você ira se tornar um especialista Fullstack')
-  }
-  escolha = prompt('Quais são as tecnologias nas quais você gostaria de se especializar ou de conhecer?')
-
-  pergunta = prompt('Tem mais alguma tecnologia que você gostaria de aprender?')
-
-  while (pergunta != 'ok') {
-    pergunta = prompt('Tem mais alguma tecnologia que você gostaria de aprender? Digite ok para sair')
-    alert('Excelente escolha')
   }
 }
 
+let fullstack = prompt("Você deseja seguir se especializando na área escolhida ou seguir se desenvolvendo para se tornar Fullstack ? Digite [1] para Especialização e [2] para Fullstack")
+ if(fullstack == 1 ){
+    alert('Parabéns você ira se tornar um especialista na linguagem escolhida escolhido')
+  } else if(fullstack == 2){
+    alert('Parabéns você ira se tornar um especialista Fullstack')
+  }
+
+
+let escolha = prompt('Quais são as tecnologias nas quais você gostaria de se especializar ou de conhecer?')
+
+let pergunta = prompt('Tem mais alguma tecnologia que você gostaria de aprender?')
+alert('Excelente escolha')
+
+while (pergunta != 'ok') {
+  pergunta = prompt('Tem mais alguma tecnologia que você gostaria de aprender? Digite ok para sair')
+    alert('Excelente escolha')
+}
 
 

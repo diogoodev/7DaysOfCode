d6f640f94fdc2d4c4f97265ededb67eabf600396
Diogo Moura <diogoodev@gmail.com>
2024-02-13 23:58:00 +0000

    add files dia-5

---
+++ logica-JS/dia-5/dia-5.js
@@ -0,0 +1,52 @@
+/**
+ * você deve criar um programa em Javascript que perguntará se você deseja adicionar uma comida na sua lista de compras, e você deve poder responder com sim ou não.
+ */
+
+let lista = [];
+let frutas =[];
+let laticínios = [];
+let congelados = [];
+let doces = [];
+let categoria = "";
+let continuar = "";
+
+
+continuar = prompt("Você deseja adicionar uma comida na sua lista de compras? responda com 'sim' ou 'não' ");
+
+/**
+ * Em seguida, ele perguntará qual comida você deseja inserir, e você digitará o nome dela, como por exemplo batata.
+ */
+
+if(continuar == 'sim'){
+  comida = prompt("Qual comida você deseja inserir?");
+  lista.push(comida);
+  console.log(lista);
+
+
+/**
+ * Depois, ele deverá perguntar em qual categoria essa comida se encaixa, com algumas opções já pré-definidas, como frutas, laticínios, congelados, doces e o que mais você achar interessante
+ */
+  categoria = prompt("Selecione, digitando o numero da categoria, em qual das categorias abaixo essa comida se encaixa?\n[1]Frutas\n[2]Laticínios\n[3]Congelados\n[4]Doces")
+
+  if(categoria == 1){
+    frutas.push(comida)
+    console.log(frutas)
+  }
+
+}
+/**
+ * caso você não queira mais adicionar nada na lista de compras e responder não na primeira pergunta, ele irá exibir uma lista com todos os itens agrupados
+ */
+
+/**
+ * Caso você adicione na sua lista:
+banana, leite em pó, tomate, leite vegetal, chiclete, bala de ursinho, maçã, uva, abacate e leite de vaca
+
+O programa deverá imprimir, por exemplo:
+
+Lista de compras:
+    Frutas: banana, tomate, maçã, uva, abacate
+    Laticínios: leite vegetal, leite de vaca, leite em pó
+    Congelados:
+    Doces: chiclete e bala de ursinho
+ */

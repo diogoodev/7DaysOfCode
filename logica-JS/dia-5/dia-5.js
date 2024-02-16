0a22c695c2bb95cf031ed9810d36c17953c61a1d
Diogo Moura <diogoodev@gmail.com>
2024-02-15 21:40:12 -0300

    add solution dia-5,js

--- logica-JS/dia-5/dia-5.js
+++ logica-JS/dia-5/dia-5.js
@@ -1,59 +1,58 @@
-/**
- * você deve criar um programa em Javascript que perguntará se você deseja adicionar uma comida na sua lista de compras, e você deve poder responder com sim ou não.
- */
-
 let lista = [];
 let frutas =[];
-let laticínios = [];
+let laticinios = [];
 let congelados = [];
 let doces = [];
 let categoria = "";
 let continuar = "";
-const fruits = document.getElementById("frutas")
+const frutashtml = document.querySelector("#frutas")
+const laticinioshtml = document.querySelector("#laticinios")
+const congeladoshtml = document.querySelector("#congelados")
+const doceshtml = document.querySelector("#doces")
 
 continuar = prompt("Você deseja adicionar uma comida na sua lista de compras? responda com 'sim' ou 'não' ");
-while (continuar == "sim"){
-
-
-/**
- * Em seguida, ele perguntará qual comida você deseja inserir, e você digitará o nome dela, como por exemplo batata.
- */
+while (continuar != 'não'){
 
   comida = prompt("Qual comida você deseja inserir?");
   lista.push(comida);
   console.log(lista);
 
-
-/**
- * Depois, ele deverá perguntar em qual categoria essa comida se encaixa, com algumas opções já pré-definidas, como frutas, laticínios, congelados, doces e o que mais você achar interessante
- */
   categoria = prompt("Selecione, digitando o numero da categoria, em qual das categorias abaixo essa comida se encaixa?\n[1]Frutas\n[2]Laticínios\n[3]Congelados\n[4]Doces")
 
   if(categoria == 1){
     frutas.push(comida)
     console.log(frutas)
+  }else if(categoria == 2){
+    laticinios.push(comida)
+    console.log(laticinios)
+  }else  if(categoria == 3){
+    congelados.push(comida)
+    console.log(congelados)
+  }else if(categoria == 4){
+    doces.push(comida)
+    console.log(doces)
+  }else{
+    alert("invalido")
   }
   continuar = prompt("Você deseja adicionar mais comida na sua lista de compras? responda com 'sim' ou 'não' ");
 }
-/**
- * caso você não queira mais adicionar nada na lista de compras e responder não na primeira pergunta, ele irá exibir uma lista com todos os itens agrupados
- */
+
+const listaFrutas = "Frutas: " + frutas.join(", "); 
+frutashtml.innerText = listaFrutas;
+
+const listaLaticinios = "Laticínios: " + laticinios.join(", "); 
+laticinioshtml.innerText = listaLaticinios;
+
+const listaCongelados = "Congelados: " + congelados.join(", "); 
+congeladoshtml.innerText = listaCongelados;
+
+const listaDoces = "Doces: " + doces.join(", "); 
+doceshtml.innerText = listaDoces;
 
 if(continuar == 'não'){
   console.log("Lista de compras")
-  for(i=0; i < frutas.length; i++){
-    console.log("\n Frutas:", frutas[i] )
+      console.log("Frutas:", frutas.join())
+      console.log("Laticínios: ", laticinios.join())
+      console.log("Congelados:", congelados.join())
+      console.log("Doces:", doces.join())
   }
-}
-/**
- * Caso você adicione na sua lista:
-banana, leite em pó, tomate, leite vegetal, chiclete, bala de ursinho, maçã, uva, abacate e leite de vaca
-
-O programa deverá imprimir, por exemplo:
-
-Lista de compras:
-    Frutas: banana, tomate, maçã, uva, abacate
-    Laticínios: leite vegetal, leite de vaca, leite em pó
-    Congelados:
-    Doces: chiclete e bala de ursinho
- */
